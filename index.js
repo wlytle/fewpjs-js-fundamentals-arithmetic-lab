ca527084c2911c7c38f0cb0f28c7911064dbcab6
wlytle <wlytle89@gmail.com>
2020-11-25 14:55:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = 24 % 5;
+
+let set = [20, 2, 3, 4, 5, 6, 8, 4, 2];
+
+const max = Math.max(...set);
